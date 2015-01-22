2ab59a48a9421b316bffba2cce3a0de846065a99
Michael Lesniak <mail@mlesniak.com>
2015-01-22 18:59:37 +0100

    Model and collection defined.

--- js/application.js
+++ js/application.js
@@ -1,5 +1,23 @@
 // Application.js
 
 $(function() {
-    console.log("Started.")
+
+    // Defines the model for a single comment.
+    var Comment = Backbone.Model.extend({
+        defaults: function() {
+            // Returns a map with fields.
+            return {
+                // No comment and author by default.
+                text: "",
+                author: ""
+            };
+        }
+    });
+
+    // List of comments for an entry.
+    var CommentList = Backbone.Collection.extend({
+        model: Comment
+    });  
+
+    var Comments = new CommentList;
 });
