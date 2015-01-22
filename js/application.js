55b86a6b2185f36ebae1c4727fc1a55cb065112d
Michael Lesniak <mail@mlesniak.com>
2015-01-23 00:02:50 +0100

    Working event handling.

--- js/application.js
+++ js/application.js
@@ -1,37 +1,26 @@
 // Application.js
 
 $(function() {
-
-    // Defines the model for a single comment.
-    var Comment = Backbone.Model.extend({
-        defaults: function() {
-            // Returns a map with fields.
-            return {
-                // No comment and author by default.
-                text: "",
-                author: ""
-            };
-        }
-    });
-
-    
-    // List of comments for an entry.
-    var CommentList = Backbone.Collection.extend({
-        model: Comment
-    });  
-    var Comments = new CommentList;
-
-    
-    // Views
+    // Views.
     var AppView = Backbone.View.extend({
         el: $("#application"),
+        dateTemplate: _.template($("#template").html()),
 
+        // Rendering.
+        render: function() {
+            $("#templatePanel").html(this.dateTemplate({
+                date: new Date()
+            }));
+            return this;
+        },
+
+        // Event handling.
         events: {
             "click #button": "buttonClicked"
         },
 
         buttonClicked: function(e) {
-            alert('Clicked')
+            this.render()
         }
     });
     var App = new AppView
